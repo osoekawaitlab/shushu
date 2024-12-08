6c8b493acebb69decbc0f4d39418959e572332d3
osoken <osoken@osoekawalab.com>
2024-12-08 23:40:09 +0900

    fix: add some options for chrome driver

--- shushu/web_agents/selenium_drivers/chrome.py
+++ shushu/web_agents/selenium_drivers/chrome.py
@@ -1,8 +1,13 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 
 from .base import BaseSeleniumDriver
 
 
 class ChromeSeleniumDriver(BaseSeleniumDriver):
     def _init_driver(self) -> None:
-        self._driver = Chrome()
+        options = Options()
+        options.add_argument("--disable-gpu")
+        options.add_argument("--headless")
+
+        self._driver = Chrome(options=options)
